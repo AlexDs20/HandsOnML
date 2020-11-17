42be9096fe1893734f665b69a26221dd77702ba6
Alexandre De Spiegeleer <alex-ds@live.be>
2020-11-17 18:49:43 +0100

    removed unnecessary files

--- Basic_Python/dataAnalysis.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python3.8
-
-import numpy as np
-# import pandas as pd
-# import matplotlib.pyplot as plt
-
-a = np.array([[1, 2, 3], [4, 5, 6]])
-
-print(a[1][1])
--- Basic_Python/test.py
+++
@@ -1,7 +0,0 @@
-#!/bin/python3.8
-
-""""Description of the file"""
-
-
-def add(a, b):
-    print(a + b)
