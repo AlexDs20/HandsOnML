243819c69c2e8ba8dee4bd710da4c3da46f5596d
Alexandre De Spiegeleer <alex-ds@live.be>
2022-11-06 20:58:42 +0100

    Everything seems to have gone through :O

--- Chapters/Chap16_NLP_with_RNN_and_Attention/transformers.py
+++ Chapters/Chap16_NLP_with_RNN_and_Attention/transformers.py
@@ -77,6 +77,33 @@ class EncoderBlock(nn.Module):
         return out
 
 
+class DecoderBlock(nn.Module):
+    def __init__(self, d_model=512, heads=8, dropout=0.1, d_ff=2048):
+        super(DecoderBlock, self).__init__()
+        self.masked_attention = MultiHeadAttention(d_model, heads, mask=True)
+        self.dropout = nn.Dropout(dropout)
+        self.norm1 = nn.LayerNorm(d_model)
+
+        self.cross_attention = MultiHeadAttention(d_model, heads)
+        self.norm2 = nn.LayerNorm(d_model)
+
+        self.ff = nn.Sequential(
+            nn.Linear(d_model, d_ff),
+            nn.ReLU(),
+            nn.Linear(d_ff, d_model)
+        )
+        self.norm3 = nn.LayerNorm(d_model)
+
+    def forward(self, outputs, K, V):
+        x = self.masked_attention(outputs, outputs, outputs)
+        Q = self.norm1(x + self.dropout(x))
+        x = self.cross_attention(Q, K, V)
+        x = self.norm2(Q + self.dropout(x))
+        ff = self.ff(x)
+        x = self.norm3(x + self.dropout(ff))
+        return x
+
+
 class PositionEmbedding(nn.Module):
     def __init__(self, max_length, d_model):
         super(PositionEmbedding, self).__init__()
@@ -102,12 +129,18 @@ class Transformer(nn.Module):
                  en_layers=6,
                  en_heads=8,
                  en_d_ff=512*4,
-                 en_max_length=100):
+                 en_max_length=100,
+                 de_vocab=1000,
+                 de_layers=6,
+                 de_heads=8,
+                 de_d_ff=512*4,
+                 de_max_length=100
+                 ):
         super(Transformer, self).__init__()
 
         self.en_embedding = nn.Embedding(en_vocab, d_model)
-        self.en_pos_embedding = PositionEmbedding(en_max_length, d_model)
-        self.en_emb_dropout = nn.Dropout(dropout)
+        self.pos_embedding = PositionEmbedding(max(en_max_length, de_max_length), d_model)
+        self.dropout = nn.Dropout(dropout)
 
         self.encoder = nn.ModuleList(
             [
@@ -116,22 +149,41 @@ class Transformer(nn.Module):
             ]
         )
 
-    def forward(self, x):
+        self.de_embedding = nn.Embedding(de_vocab, d_model)
+        self.decoder = nn.ModuleList(
+            [
+                DecoderBlock(d_model, de_heads, dropout, de_d_ff)
+                for _ in range(de_layers)
+            ]
+        )
+
+        self.linear = nn.Linear(d_model, de_vocab)
+
+    def forward(self, x, dec_in):
         # Encoder part
         x = self.en_embedding(x)
-        x = x + self.en_pos_embedding(x)
+        x = self.dropout(x + self.pos_embedding(x))
 
         for encoder_layer in self.encoder:
             x = encoder_layer(x)
 
-        return x
         # Decoder part
+        dec_in = self.de_embedding(dec_in)
+        dec_in = self.dropout(dec_in + self.pos_embedding(dec_in))
+
+        for decoder_layer in self.decoder:
+            dec_in = decoder_layer(dec_in, x, x)
+
+        out = self.linear(dec_in)
+
+        return out
 
 
 if __name__ == '__main__':
+    device = torch.device('cpu' if not torch.cuda.is_available() else 'cuda')
     # Test positional Embedding
-    input = torch.randint(10, (4, 20))
-    pe = PositionEmbedding(30, 100)
+    input = torch.randint(10, (4, 20)).to(device)
+    pe = PositionEmbedding(30, 100).to(device)
     print(pe(input).shape)
 
     # Test Encoder block
@@ -139,7 +191,16 @@ if __name__ == '__main__':
     eb = EncoderBlock()
     print(eb(input).shape)
 
+    # Test Decoder block
+    outputs = torch.rand(4, 20, 512)
+    K = torch.rand((4, 20, 512))
+    V = torch.rand((4, 20, 512))
+    db = DecoderBlock()
+    print(db(outputs, K, V).shape)
+
     # Test transformer
     input = torch.randint(10, (4, 20))
+    dec = torch.randint(15, (4, 22))
     tf = Transformer()
-    print(tf(input).shape)
+    print(tf(input, dec).shape)
+
