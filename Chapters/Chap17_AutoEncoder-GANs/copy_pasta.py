8346a1a31b69af075526a2d2a77228369648ca6d
Alexandre De Spiegeleer <alexandre.de.spiegeleer@husqvarnagroup.com>
2023-01-05 16:11:51 +0100

    Adapted to vae

--- Chapters/Chap17_AutoEncoder-GANs/copy_pasta.py
+++ Chapters/Chap17_AutoEncoder-GANs/copy_pasta.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt; plt.rcParams['figure.dpi'] = 200
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+
 class Encoder(nn.Module):
     def __init__(self, latent_dims):
         super(Encoder, self).__init__()
@@ -35,6 +36,37 @@ class Autoencoder(nn.Module):
     def __init__(self, latent_dims):
         super(Autoencoder, self).__init__()
         self.encoder = Encoder(latent_dims)
+        self.decoder = Decoder(latent_dims)
+
+    def forward(self, x):
+        z = self.encoder(x)
+        return self.decoder(z)
+
+class VariationalEncoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(VariationalEncoder, self).__init__()
+        self.linear1 = nn.Linear(784, 512)
+        self.linear2 = nn.Linear(512, latent_dims)
+        self.linear3 = nn.Linear(512, latent_dims)
+
+        self.N = torch.distributions.Normal(0, 1)
+        self.N.loc = self.N.loc.cuda() # hack to get sampling on the GPU
+        self.N.scale = self.N.scale.cuda()
+        self.kl = 0
+
+    def forward(self, x):
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.linear1(x))
+        mu =  self.linear2(x)
+        sigma = torch.exp(self.linear3(x))
+        z = mu + sigma*self.N.sample(mu.shape)
+        self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 1/2).sum()
+        return z
+
+class VariationalAutoencoder(nn.Module):
+    def __init__(self, latent_dims):
+        super(VariationalAutoencoder, self).__init__()
+        self.encoder = VariationalEncoder(latent_dims)
         self.decoder = Decoder(latent_dims)
 
     def forward(self, x):
@@ -83,7 +115,8 @@ def plot_reconstructed(autoencoder, r0=(-5, 10), r1=(-10, 5), n=12):
     plt.close()
 
 latent_dims = 2
-autoencoder = Autoencoder(latent_dims).to(device) # GPU
+#autoencoder = Autoencoder(latent_dims).to(device) # GPU
+autoencoder = VariationalAutoencoder(latent_dims).to(device)
 
 data = torch.utils.data.DataLoader(
         torchvision.datasets.MNIST('../Chap16_NLP_with_RNN_and_Attention/data/',
